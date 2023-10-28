72b29fe77ab2364cdde6714e7c7a7239d363fde9
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-10-28 11:58:08 +0400

    feat: add chart sample

--- src/pages/GlobalOverview.tsx
+++ src/pages/GlobalOverview.tsx
@@ -1,7 +1,240 @@
-const GlobalOverview = () => {
+import { Box, Container } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import React from "react";
+
+const GlobalOverview: React.FC = () => {
+  const data = Array.from({ length: 24 }, () => Math.round(Math.random()));
+
   return (
-    <div>GlobalOverview</div>
-  )
-}
+    <Container maxWidth="xl">
+      <Grid container columnSpacing={2} rowSpacing={2} paddingY="3rem">
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="30px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          sx={{
+            borderRadius: "0.5rem",
+            background: "#2B2E31",
+            boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%"
+            padding="1rem"
+          >
+            {data.map((value, index) => (
+              <Box
+                key={index}
+                width="2%"
+                height="60px"
+                bgcolor={value ? "#7FCD9F" : "#E93F3F"}
+                mx={0.1}
+              />
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default GlobalOverview;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,16 @@
+import { lazy } from "react";
 import Dashboard from "../pages/Dashboard";
-// const 
+const GlobalOverview = lazy(() => import("../pages/GlobalOverview"));
 
 export const mainRoutes = [
   {
     path: "/",
     element: <Dashboard />,
     title: "Dashboard",
+  },
+  {
+    path: "/global-overview",
+    element: <GlobalOverview />,
+    title: "Overview",
   },
 ];
