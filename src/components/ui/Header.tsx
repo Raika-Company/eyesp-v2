54a6029d1c2b2eb26b428621216e26eff1d7e9b9
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-11-07 12:12:55 +0330

    add new header

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,149 @@
+import { FC, ReactNode, useState } from "react";
+import ArrowLeft from "../../assets/images/arrow-left.svg";
+import { Box, FormControl, MenuItem, Typography } from "@mui/material";
+import { SelectButton } from "./SelectButton";
+
+interface Props {
+  title: string;
+  selectTitle: string;
+  iconPath: string;
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const Header: FC<Props> = ({
+  title,
+  iconPath,
+  children,
+  onClick,
+  selectTitle,
+}) => {
+  const [age, setAge] = useState("در حال حاضر");
+
+  return (
+    <Box
+      sx={{
+        flexGrow: "1",
+        // borderRadius: ".5rem",
+        boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
+        background: "#2B2E31",
+        border: "1px solid #2B2E31",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          background: "#232629",
+          borderTopRightRadius: ".5rem",
+          borderTopLeftRadius: ".5rem",
+          paddingX: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: "1",
+            alignItems: "center",
+            gap: "1.18rem",
+          }}
+        >
+          <img alt="iconPath" src={iconPath} />
+          <Typography>{title}</Typography>
+          <Typography color="#7A7775" mr="3rem">
+            {selectTitle}
+          </Typography>
+
+          <FormControl
+            sx={{ width: "8%", height: "70px", marginTop: "1.8rem" }}
+          >
+            <SelectButton
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="سال"
+              //   onChange={handleChangeDailyPercent}
+              displayEmpty
+            >
+              <MenuItem value="در حال حاضر">درحال حاضر</MenuItem>
+              <MenuItem value="۳ ساعت پیش">۳ ساعت پیش</MenuItem>
+              <MenuItem value="امروز">امروز</MenuItem>
+              <MenuItem value="دیروز">دیروز</MenuItem>
+              <MenuItem value="هفتگی">هفتگی</MenuItem>
+              <MenuItem value="ماهانه">ماهانه</MenuItem>
+              <MenuItem value="سالانه">سالانه</MenuItem>
+            </SelectButton>
+          </FormControl>
+          <FormControl
+            sx={{
+              width: "8%",
+              height: "70px",
+              marginTop: "1.8rem",
+            }}
+          >
+            <SelectButton
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="سال"
+              //   onChange={handleChangeDailyPercent}
+              displayEmpty
+            >
+              <MenuItem value="در حال حاضر">درحال حاضر</MenuItem>
+              <MenuItem value="۳ ساعت پیش">۳ ساعت پیش</MenuItem>
+              <MenuItem value="امروز">امروز</MenuItem>
+              <MenuItem value="دیروز">دیروز</MenuItem>
+              <MenuItem value="هفتگی">هفتگی</MenuItem>
+              <MenuItem value="ماهانه">ماهانه</MenuItem>
+              <MenuItem value="سالانه">سالانه</MenuItem>
+            </SelectButton>
+          </FormControl>
+          <FormControl
+            sx={{ width: "8%", height: "70px", marginTop: "1.8rem" }}
+          >
+            <SelectButton
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="سال"
+              //   onChange={handleChangeDailyPercent}
+              displayEmpty
+            >
+              <MenuItem value="در حال حاضر">درحال حاضر</MenuItem>
+              <MenuItem value="۳ ساعت پیش">۳ ساعت پیش</MenuItem>
+              <MenuItem value="امروز">امروز</MenuItem>
+              <MenuItem value="دیروز">دیروز</MenuItem>
+              <MenuItem value="هفتگی">هفتگی</MenuItem>
+              <MenuItem value="ماهانه">ماهانه</MenuItem>
+              <MenuItem value="سالانه">سالانه</MenuItem>
+            </SelectButton>
+          </FormControl>
+        </Box>
+        <Box display="flex" alignItems="center" gap={2}>
+          <Typography>بازگشت</Typography>
+          <img
+            alt="arrowLeft"
+            onClick={onClick}
+            src={ArrowLeft}
+            style={{
+              marginLeft: ".5rem",
+              cursor: "pointer",
+            }}
+          />
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          marginY: "auto",
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import Dashboard from "../pages/Dashboard";
 import GlobalOverview from "../pages/GlobalOverview";
 import ISP from "../pages/ISP";
 import Disorders from "../pages/Disorders";
+import LastDisorders from "../pages/lastDisorders/LastDisorders";
 
 export const mainRoutes = [
   {
@@ -24,5 +25,10 @@ export const mainRoutes = [
     path: "/isp",
     element: <ISP />,
     title: "ISP",
-  }
+  },
+  {
+    path: "/last-disorders",
+    element: <LastDisorders />,
+    title: "lastDisorders",
+  },
 ];
