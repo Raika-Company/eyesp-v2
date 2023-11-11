be4290c021fff84644e2156efb1c8ac1f41b8b01
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-11-11 15:37:55 +0330

    fix the history disorder

--- src/features/disorderHistory/DisorderHistory.tsx
+++ src/features/disorderHistory/DisorderHistory.tsx
@@ -1,48 +1,37 @@
 import Header from "../../components/ui/Header";
-import {
-  Box,
-  Divider,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  styled,
-} from "@mui/material";
 import history from "../../assets/images/history.svg";
 import CustomTable from "../../components/ui/CustomTable";
+import { useState } from "react";
+import { SelectChangeEvent } from "@mui/material";
 const cellHeaders = ["تاریخ و ساعت", "نوع اختلال", "دلیل اختلال", "وضعیت"];
 
-const RowBox = styled(Box)(() => ({
-  display: "flex",
-  alignItems: "center",
-  borderRadius: "1rem",
-  paddingRight: "3rem",
-  "& > *": {
-    flex: 1,
-    fontFamily: "PeydaLight",
-  },
-  "& > *:nth-of-type(1)": {
-    flex: 0.5,
-  },
-  "& > *:nth-of-type(4)": {
-    flex: 0.3,
-  },
-}));
-const HorizontalLine = styled(Box)(() => ({
-  margin: "0 auto",
-  width: "400px",
-  height: "2px",
-  background:
-    "linear-gradient(90deg,rgba(255, 255, 255, 0) 0%,rgb(255, 255, 255) 49.48%,rgba(255, 255, 255, 0) 100%)",
-  opacity: "0.2",
-}));
 const DisorderHistory = () => {
+  const [province, setProvince] = useState("");
+  const [selectedISP, setSelectedISP] = useState("");
+  const [category, setCategory] = useState("");
+  // const [visibleRows, setVisibleRows] = useState(6);
+
+  const handleCategory = (event: SelectChangeEvent<unknown>) => {
+    setCategory(event.target.value as string);
+  };
+  const handleProvinceChange = (event: SelectChangeEvent<unknown>) => {
+    setProvince(event.target.value as string);
+  };
+  const handleISPChange = (event: SelectChangeEvent<unknown>) => {
+    setSelectedISP(event.target.value as string);
+  };
   return (
     <div>
       <Header
         title="تاریخچه اختلالات"
         iconPath={history}
         selectTitle="ترتیب بندی براساس:"
+        handleISPChange={handleISPChange}
+        handleProvinceChange={handleProvinceChange}
+        handleCategory={handleCategory}
+        category={category}
+        province={province}
+        selectedISP={selectedISP}
         // onClick={toggleDialog}
       ></Header>
       <CustomTable cellHeaders={cellHeaders} />
