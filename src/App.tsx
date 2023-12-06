fb4e331d1c9f26bc1321b7eafda1e6b148136e87
AliNikseresht <alinksht@gmail.com>
2023-12-06 09:38:12 +0330

    fix: fix error in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { FC, Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 import { mainRoutes, privateMainRoutes } from "./routes/Routes";
-import { FC, Suspense } from "react";
 import "./layout/global.css";
 import theme from "./layout/theme";
 import { MessageProvider } from "./context/MessageContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const queryClient = new QueryClient({});
 const DASHBOARD_PATH = "/";
 const DASHBOARD_PATH_PRIVATE = "/private";
 
 const App: FC = () => {
-  // const location = useLocation();
-  // const showSuspense =
-  //   location.pathname !== DASHBOARD_PATH &&
-  //   location.pathname !== DASHBOARD_PATH_PRIVATE;
+  const AppContent: FC = () => {
+    const location = useLocation();
+    const showSuspense =
+      location.pathname !== DASHBOARD_PATH &&
+      location.pathname !== DASHBOARD_PATH_PRIVATE;
+
+    return showSuspense ? (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {mainRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+          {privateMainRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Suspense>
+    ) : (
+      <Routes>
+        {mainRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+        {privateMainRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    );
+  };
 
   return (
     <MessageProvider>
@@ -23,43 +47,7 @@ const App: FC = () => {
         <CssBaseline />
         <QueryClientProvider client={queryClient}>
           <Router>
-            {/* {showSuspense ? (
-              <Suspense fallback={<div>loading</div>}> */}
-            <Routes>
-              {mainRoutes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-              {privateMainRoutes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-            </Routes>
-            {/* </Suspense>
-            ) : ( */}
-            <Routes>
-              {mainRoutes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-              {privateMainRoutes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-            </Routes>
-            {/* )} */}
+            <AppContent />
           </Router>
         </QueryClientProvider>
       </ThemeProvider>
