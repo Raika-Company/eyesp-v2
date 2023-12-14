9aaf1c9ebb402f5e7fbfa49c701473319c786b2b
AliNikseresht <alinksht@gmail.com>
2023-12-14 12:07:32 +0330

    fix: api in global-overview page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/get-history": "http://2.189.59.122:3000",
+      "/get-history": "http://2.189.59.122:3000", // existing proxy setup
+      "/API": "http://185.11.89.120:6969" // add this line to proxy the new API
     },
   },
   plugins: [react()],
