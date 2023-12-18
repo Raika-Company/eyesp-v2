daf17bb06bb96d9f53e382ad1231e3f25cf980eb
AliNikseresht <alinksht@gmail.com>
2023-12-18 07:54:04 +0330

    fix: add new api for global-overview

--- src/pages/public/GlobalOverview.tsx
+++ src/pages/public/GlobalOverview.tsx
@@ -13,16 +13,7 @@ import {
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Link, useNavigate } from "react-router-dom";
 import WestIcon from "@mui/icons-material/West";
-import { convertToPersianDate } from "../../utils/convertToPersianDate";
 import serverStatusData from '../../../public/data/server_status.json';
-
-
-interface DataBlockProps {
-  value: number; // The HTTP status code of the website check.
-  checkTime: string; // The time at which the check was performed.
-  data: WebsiteData; // Data about the website.
-  statusMessage?: string; // Optional status message.
-}
 
 
 type StatusDetail = {
@@ -42,184 +33,52 @@ type WebsiteData = {
   hourly_status: HourlyStatus[];
 };
 
-
-
-/**
- * Represents a single history item with status code and check time.
- */
-type HistoryItem = {
-  status: number; // HTTP status code of the website check.
-  check_time: string; // Time at which the check was performed, in ISO format.
+const getHourlyStatusColor = (hourlyDetails: StatusDetail[]) => {
+  const non200Count = hourlyDetails.filter(detail => parseInt(detail.status, 10) !== 200).length;
+  if (non200Count > 4) return "#E93F3F";
+  if (non200Count >= 2) return "#f19e2c";
+  return "#7FCD9F";
 };
 
-/**
- * Represents a period of outage with start and end times.
- */
-interface OutagePeriod {
-  start: string; // Start time of the outage period.
-  end: string; // End time of the outage period.
-}
+const getTooltipMessage = (hourlyDetails: StatusDetail[], color: string) => {
+  const non200Details = hourlyDetails.filter(detail => parseInt(detail.status, 10) !== 200);
+  let tooltipContent = [];
 
-export const REFRESH_INTERVAL = 60000;
-
-
-const getStatusMessage = (statusCode: number): string =>
-  errorMessages[statusCode] || "یک خطای ناشناخته رخ داده است.";
-
-const getTitleMessage = (statusCode: number): string =>
-  errorTitel[statusCode] || "عنوان خطای ناشناخته";
-
-
-const errorMessages: Record<number, string> = {
-  200: "درخواست با موفقیت انجام شد.",
-  401: "دسترسی به صورت موقت قطع شده است لطفا دقایقی بعد تلاش کنید.",
-  403: "دسترسی به صورت موقت قطع شده است لطفا بعدا تلاش کنید.",
-  404: "صفحه مورد نظر یافت نشد.",
-  500: "سرویس موقتا در دسترس نیست. لطفا دقایقی بعد تلاش کنید.",
-  503: "سرویس موقتا در دسترس نیست. لطفا دقایقی بعد تلاش کنید.",
-};
-
-const errorTitel: Record<number, string> = {
-  200: "✅ بدون قطعی",
-  401: "❌ قطعی کامل سرویس",
-  403: "❌ قطعی کامل سرویس",
-  404: "❌ قطعی کامل سرویس",
-  500: "⚠️ قطعی جزئي",
-  503: "⚠️ قطعی جزئی",
-};
-
-/**
- * Generates a status message based on status code and outage period.
- * @param statusCode - HTTP status code.
- * @param startTime - Start time of the outage.
- * @param endTime - End time of the outage.
- * @param isOngoing - Flag indicating if the outage is ongoing.
- * @returns A string message detailing the status.
- */
-const generateStatusMessage = (
-  statusCode: number,
-  startTime: string,
-  endTime: string
-): string => {
-  if (statusCode === 200) {
-    return "";
-  }
-
-  const formattedStartTime = convertToPersianDate(startTime);
-  const formattedEndTime = convertToPersianDate(endTime);
-  return `قطعی سرویس از ${formattedStartTime} تا ${formattedEndTime}`;
-};
-
-/**
- * Finds outage periods based on the history of status checks.
- * @param history - Array of history items for a website.
- * @returns Array of OutagePeriod objects indicating the outage periods.
- */
-const findOutagePeriods = (history: HistoryItem[]): OutagePeriod[] => {
-  const outagePeriods: OutagePeriod[] = [];
-  let outageStart: string | null = null;
-
-  history.forEach((item) => {
-    if (item.status !== 200 && !outageStart) {
-      outageStart = item.check_time;
-    } else if (item.status === 200 && outageStart) {
-      outagePeriods.push({
-        start: outageStart,
-        end: item.check_time,
-      });
-      outageStart = null;
-    }
-  });
-
-  if (outageStart) {
-    outagePeriods.push({
-      start: outageStart,
-      end: history[history.length - 1].check_time,
-    });
-  }
-
-  return outagePeriods;
-};
-
-/**
- * Functional component to display a single data block representing the status
- * of a website check at a specific time.
- *
- * @param value - The HTTP status code of the website check.
- * @param checkTime - The time at which the check was performed.
- */
-const DataBlock: React.FC<DataBlockProps> = React.memo(({ value, statusMessage }) => {
-  const errorMessage = getStatusMessage(value);
-  const statusTitle = getTitleMessage(value);
-
-  let bgColor = "#E93F3F";
-  if (value === 200) {
-    bgColor = "#7FCD9F";
-  } else if (value === 403) {
-    bgColor = "#E93F3F";
-  } else if (value === 503 || value === 500) {
-    bgColor = "#f19e2c";
+  if (color === "#E93F3F") {
+    const start = non200Details[0].time;
+    const end = non200Details[non200Details.length - 1].time;
+    tooltipContent.push(
+      <Typography sx={{ color: color, fontSize: "1.3rem", py: "0.5em", px: "0.5em" }}>❌ قطعی کامل سرویس</Typography>,
+      <Typography sx={{ bgcolor: '#2B2E31', p: "1em" }}>قطعی کامل سرویس از ساعت {start}  تا ساعت {end}</Typography>,
+      <Typography sx={{ mt: "0.5em" }}>دسترسی به صورت موقت قطع شده است لطفا بعدا تلاش کنید.</Typography>
+    );
+  } else if (color === "#7FCD9F") {
+    tooltipContent.push(
+      <Typography sx={{ color: color, fontSize: "1.3rem", mt: "0.2em" }}>وضعیت : 200</Typography>,
+      <Typography sx={{ bgcolor: '#2B2E31', py: "0.5em", px: "0.5em", color: color, fontSize: "1.3rem", mt: "0.2em" }}>✅ بدون قطعی</Typography>,
+      <Typography sx={{ mt: "0.2em", mx: "0.2em" }}>درخواست با موفقیت انجام شد.</Typography>
+    );
+  } else if (color === "#f19e2c" && non200Details.length > 0) {
+    const start = non200Details[0].time;
+    const end = non200Details[non200Details.length - 1].time;
+    tooltipContent.push(
+      <Typography sx={{ color: color, fontSize: "1.3rem", my: "0.4rem" }}>⚠️ اختلال جزئی در اتصال</Typography>,
+      <Typography sx={{ bgcolor: '#2B2E31', py: "0.5em", px: "0.5em" }}>اختلال جزئی از ساعت {start} تا ساعت {end}</Typography>,
+      <Typography sx={{ mt: "0.5em" }}>سرویس موقتا در دسترس نیست. لطفا دقایقی بعد تلاش کنید.</Typography>
+    );
   }
 
   return (
-    <Tooltip
-      arrow
-      title={
-        <Box sx={{ p: "0.3em", userSelect: "none" }}>
-          <Typography color={bgColor} fontSize="1.3rem">
-            وضعیت: {value}
-          </Typography>
-          <Typography
-            sx={{
-              my: "0.5em",
-              bgcolor: "#777777",
-              p: ".4em",
-              borderRadius: ".2em",
-              fontSize: "1.2rem",
-            }}
-          >
-            {statusMessage && (
-              <Typography sx={{ mt: "0.5em" }}>{statusMessage}</Typography>
-            )}
-            {statusTitle}:
-            <Typography>{errorMessage}</Typography>
-          </Typography>
-        </Box>
-      }
-    >
-      <Box
-        bgcolor={bgColor}
-        my={0.3}
-        sx={{
-          width: "10px",
-          height: "50px",
-          mx: "0.1em",
-          cursor: "pointer",
-          borderRadius: '2em',
-          "&:hover": {
-            bgcolor: "darkgray",
-          },
-        }}
-      />
-    </Tooltip>
+    <Typography>
+      {tooltipContent.map((content, index) => (
+        <React.Fragment key={index}>{content}</React.Fragment>
+      ))}
+    </Typography>
   );
-});
+};
 
-
-/**
- * Functional component to display information about a single website
- * including its history of status checks.
- * @param data - Data pertaining to a single website.
- */
 const GridItem: React.FC<{ data: WebsiteData }> = ({ data }) => {
-  const allStatuses = data.hourly_status.flatMap(hourlyStatus =>
-    hourlyStatus.details.map(detail => ({
-      status: parseInt(detail.status, 10),
-      check_time: `${data.date}T${hourlyStatus.hour}:${detail.time}`
-    }))
-  );
 
-  const outagePeriods = findOutagePeriods(allStatuses);
   return (
     <Grid
       xs={12}
@@ -260,22 +119,31 @@ const GridItem: React.FC<{ data: WebsiteData }> = ({ data }) => {
         height="100%"
         padding="1rem"
       >
-        {allStatuses.slice(-24).map((item, index) => {
-          const statusMessage = item.status !== 200
-            ? outagePeriods
-              .filter(period => item.check_time >= period.start && item.check_time <= period.end)
-              .map(period => generateStatusMessage(item.status, period.start, period.end))
-              .join(", ") || "وضعیت نامشخص"
-            : "";
+        {data.hourly_status.slice(-24).map((hourlyStatus, index) => {
+          const bgColor = getHourlyStatusColor(hourlyStatus.details);
+          const statusMessage = getTooltipMessage(hourlyStatus.details, bgColor);
 
           return (
-            <DataBlock
+            <Tooltip
               key={index}
-              value={item.status}
-              checkTime={item.check_time}
-              data={data}
-              statusMessage={statusMessage}
-            />
+              title={<Typography>{statusMessage}</Typography>}
+              arrow
+            >
+              <Box
+                bgcolor={bgColor}
+                my={0.3}
+                sx={{
+                  width: "10px",
+                  height: "50px",
+                  mx: "0.1em",
+                  cursor: "pointer",
+                  borderRadius: '2em',
+                  "&:hover": {
+                    bgcolor: "darkgray",
+                  }
+                }}
+              />
+            </Tooltip>
           );
         })}
       </Box>
@@ -283,10 +151,6 @@ const GridItem: React.FC<{ data: WebsiteData }> = ({ data }) => {
   )
 };
 
-/**
- * Component for rendering the global overview of website statuses.
- * It fetches and displays data for multiple websites and their historical statuses.
- */
 const GlobalOverview: React.FC = () => {
   const theme = useTheme();
   const [showModal, setShowModal] = useState(false);
