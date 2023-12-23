bc04ee8d0ef7f065e0c718dea9a21a68dc1eec4b
mahjs <mahjsdev@gmail.com>
2023-12-23 10:58:11 +0330

    make ui responsiveness bugs fixed

--- src/components/ui/InfoBox.tsx
+++ src/components/ui/InfoBox.tsx
@@ -21,6 +21,7 @@ const InfoBox: FC<Props> = ({
     <Box
       sx={{
         position: "relative",
+        flexBasis: "17.5rem",
         flexGrow: "1",
         borderRadius: ".5rem",
         boxShadow: "0px 12px 17px 0px rgba(0, 0, 0, 0.60)",
@@ -28,7 +29,8 @@ const InfoBox: FC<Props> = ({
         border: "1px solid #2B2E31",
         display: "flex",
         flexDirection: "column",
-        minHeight: "12rem",
+        minHeight: "15rem",
+        minWidth: "19rem",
       }}
     >
       <Box
