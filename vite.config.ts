9303690bf08364e93a11630a3cf79759531d8600
AliNikseresht <alinksht@gmail.com>
2024-02-05 10:47:28 +0330

    fix:add new api for global-overview

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      "/get-history": "http://2.189.59.122:3000",
-    },
-  },
-  plugins: [react()],
+      '/api/v1/analysis/result': {
+        target: 'http://95.38.58.41:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1\/analysis\/result/, '/api/v1/analysis/result')
+      }
+    }
+  }
 });
