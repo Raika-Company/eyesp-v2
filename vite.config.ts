0eb8d7d07be64320970ef710512f0ca88b597c0e
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2024-02-19 12:53:02 +0400

    fix: server address port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://95.38.58.11:8000",
+        target: "http://95.38.58.11:80",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/v1/, ""),
       }
